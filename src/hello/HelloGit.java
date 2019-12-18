b1075fb96d0f4d0ea9535cdd67e443ab17dc4a4f
Jam <wuguojiang170807@163.com>
2019-12-18 16:14:46 +0800

    First commit all.

---
+++ src/hello/HelloGit.java
@@ -0,0 +1,13 @@
+package hello;
+
+/**
+ * @author: Jam
+ * @create: 2019-12-18 16:08
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git, I come from IDEA.");
+    }
+
+}
