0a7c611424018510ea9f1551fc4e7c1142acdd39
Jam <wuguojiang170807@163.com>
2019-12-18 16:18:28 +0800

    First commit all.

--- src/hello/HelloGit.java
+++ src/hello/HelloGit.java
@@ -7,7 +7,7 @@ package hello;
 public class HelloGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git, I come from IDEA.");
+        System.out.println(" Hello Git, I come from IDEA.");
     }
 
 }
