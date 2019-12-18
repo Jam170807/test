ff276e07d52a39b7da5fe352c4529ffe0d0500b4
Jam <wuguojiang170807@163.com>
2019-12-18 16:37:12 +0800

    第三次提交

--- src/hello/HelloGit.java
+++ src/hello/HelloGit.java
@@ -1,13 +1,15 @@
 package hello;
 
 /**
+ * @description: 上传到Git的测试代码2.0
  * @author: Jam
  * @create: 2019-12-18 16:08
  */
 public class HelloGit {
 
     public static void main(String[] args) {
-        System.out.println(" Hello Git, I come from IDEA.");
+        System.out.println("Hello Git, I come from IDEA.");
+        System.out.println("Welcome! Nice to meet you.");
     }
 
 }
